c369524f91872d48bd780f8bd098452e53f6fc27
brgarcias <bruno-151299@hotmail.com>
2021-02-17 23:01:08 -0300

    feat: tipos de encapsulamento

--- src/app/exercicio-ngclass/exercicio-ngclass.component.ts
+++ src/app/exercicio-ngclass/exercicio-ngclass.component.ts
@@ -3,7 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-exercicio-ngclass',
   templateUrl: './exercicio-ngclass.component.html',
-  styleUrls: ['./exercicio-ngclass.component.scss']
+  styleUrls: ['./exercicio-ngclass.component.scss'],
+  // Padrão
+  // encapsulation: ViewEncapsulation.Emulated,
+  
+  // Sem encapsulamento
+  // encapsulation: ViewEncapsulation.None,
+  
+  // 
+  // encapsulation: ViewEncapsulation.ShadowDom
 })
 export class ExercicioNgclassComponent implements OnInit {
 
