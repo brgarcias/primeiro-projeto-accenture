5f8e1fd907dc00f2f6c7a8e0c00dd7f535164c2b
brgarcias <bruno-151299@hotmail.com>
2021-02-20 01:22:47 -0300

    feat: chamadas post, put e delete

--- src/app/contato/contato.service.ts
+++ src/app/contato/contato.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { timer } from 'rxjs';
 import { mergeMap } from 'rxjs/operators';
@@ -12,11 +12,16 @@ import { Contatos } from './contato.interfaces';
 export class ContatoService {
 
   API_URL = environment.API_URL;
+  httpOptions = {
+    headers: new HttpHeaders({
+      Authorization: '.... TOKEN DE AUTENTICAÇÃO ....'
+    })
+  }
 
   constructor(
     private http: HttpClient
   ) { }
-  
+
   getContatos(page: number) {
     // const error = throwError(new Error('Erro genérico.'))
     const api = this.http.get<Contatos[]>(`${this.API_URL}/contatos`, {
@@ -28,19 +33,27 @@ export class ContatoService {
       .pipe(
         mergeMap(() => api)
       )
-    
+
   }
-  
+
   getContato(id: string) {
     // const error = throwError(new Error('Erro genérico.'))
-    const api = this.http.get<Contatos>(`${this.API_URL}/contatos/${id}`, {});
+    const api = this.http.get<Contatos>(`${this.API_URL}/contatos/${id}`, this.httpOptions);
     return timer(500)
       .pipe(
         mergeMap(() => api)
       )
-    
   }
 
+  createContato(contato: Contatos) {
+    return this.http.post<Contatos[]>(`${this.API_URL}/contatos`, contato, this.httpOptions);
+  }
 
-  
+  updateContato(id: string, contato: Contatos) {
+    return this.http.put<Contatos[]>(`${this.API_URL}/contatos` + id, contato, this.httpOptions);
+  }
+
+  deleteContato(id: string) {
+    return this.http.delete<Contatos[]>(`${this.API_URL}/contatos/${id}`, this.httpOptions);
+  }
 }
